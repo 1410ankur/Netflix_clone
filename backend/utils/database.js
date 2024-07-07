d7169be51e6ff296959bd0eda6f77c9a669c7ee3
Ankur <ankurrao381@gmail.com>
2024-07-07 19:13:57 +0530

    commit branch

---
+++ backend/utils/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"../.env"
+})
+
+const databaseConnection = () => {
+    mongoose.connect("mongodb+srv://ankurrao381:ByvWDF3HQFMbeRB0@cluster0.etb8hc8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
+        useNewUrlParser: true,
+  useUnifiedTopology: true,
+    }).then(()=>{
+        
+        console.log("mongodb connected successfully");
+    }).catch((error)=>{
+        console.error('Error connecting to MongoDB:', error);
+    })
+};
+export default databaseConnection;
